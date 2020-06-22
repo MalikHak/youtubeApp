19acd239bc2d6e08150926f306e80e008e69e4c9
faisalhakim <malikfaisalhakim@gmail.com>
2020-06-22 20:15:25 +0430

    First Commit

---
+++ AfghaniTube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AfghaniTube
+//
+//  Created by Faisal Hakim on 6/22/20.
+//  Copyright © 2020 Faisal Hakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
