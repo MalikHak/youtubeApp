aa0a9cc74a049e014e96d535c080cb7e937a5d84
faisalhakim <malikfaisalhakim@gmail.com>
2020-06-24 18:00:12 +0430

    added tableView,Cell, Item,

--- AfghaniTube/Model.swift
+++ AfghaniTube/Model.swift
@@ -7,20 +7,27 @@
 //
 
 import Foundation
+protocol ModelDelegate {
+    func videosFetched(_ videos: [Video])
+}
+
 
 class Model {
     
+    var delegate: ModelDelegate?
     func getVideos() {
    
-//Create url Object
+         //Create url Object
         let url = URL(string: Constatns.API_URL)
-
         guard url != nil else {
             return
         }
-
+        
+        
+        
         //Get a URL Session Object
         let session = URLSession.shared
+        
         
         //Get a task from the url session object
         let dataTask = session.dataTask(with: url!){(data,resoponse,error) in
@@ -29,26 +36,26 @@ class Model {
             if error != nil || data == nil{
                 return
             }
-            
             //Parsing the data into video Objects
-            
-      
-            
             do{
                 let decoder = JSONDecoder()
                 
                 decoder.dateDecodingStrategy = .iso8601
                 let response = try decoder.decode(Response.self, from: data!)
+  
+             //Call the videoFetched  method  of delegate
+                if response.items != nil{
+                    
+                    DispatchQueue.main.async {
+                        self.delegate?.videosFetched(response.items! )
+                    }
 
+                }
                 
-                dump(response)
+             //   dump(response)
             }catch{ 
                 
             }
-            
-            
-            
-            
         }
         //Kick of the task
         dataTask.resume()
--- AfghaniTube/ViewController.swift
+++ AfghaniTube/ViewController.swift
@@ -8,17 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource ,UITableViewDelegate, ModelDelegate {
 
+    
+
+    var videos = [Video]()
+
+    @IBOutlet weak var uiTableView: UITableView!
     var model = Model()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        uiTableView.dataSource = self
+        uiTableView.delegate = self
+        
+        
+        //set itself as the delegate of the model
+        
+        model.delegate = self
+        
         model.getVideos()
 
     }
+    
+    
+    
+    func videosFetched(_ videos: [Video]) {
 
+        
+        //set the returned videos to our videos property
+        
+        self.videos = videos
+        
+         //Refresh the table View
+        
+        uiTableView.reloadData()
+        
+        
+    }
+    
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constatns.VIDEO_ID, for: indexPath)
+        
+        
+        //Configure the cell with the data
+        // Get the title for the video in question
+        
+        let title = self.videos [indexPath.row].title
+        
+        cell.textLabel?.text = title
+        
+        
+        //Return the cell
+        return cell 
+    
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
 
 }
 
